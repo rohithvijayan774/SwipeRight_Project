47a8f191fb59a1a7dacff573068de6df2ba23451
Rohith Vijayan <rohithvijayan774@gmail.com>
2023-12-06 17:34:28 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:swiperight/firebase_options.dart';
 import 'package:swiperight/views/welcome_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:const WelcomePage(),
+      home: const WelcomePage(),
     );
   }
 }
